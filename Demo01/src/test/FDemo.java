c8248868a07eda491a74bd5d5cb043721be55141
Divyanshu84 <56114465+Divyanshu84@users.noreply.github.com>
2021-08-16 15:15:19 +0530

    Add files via upload

---
+++ Demo01/src/test/FDemo.java
@@ -0,0 +1,46 @@
+package test;
+
+public class FDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * int i =10; System.out.println("The value of i is := "+i);
+		 * 
+		 * char c= 'a';
+		 * 
+		 * System.out.println("         "+c );
+		 * 
+		 * String s= "Hi";
+		 * 
+		 * System.out.println(s);
+		 * 
+		 */
+		
+		//int a=10, b=20;
+		int a=25;
+		int b=200;
+		int c=30; 
+		
+		if(a>b && a>c) // True 
+		{
+			System.out.println("A is Gr" +a);
+		}
+		
+		
+		else if(b>c)
+		{
+			System.out.println("B is Gr "+b);
+		}
+		
+		else
+		{
+			System.out.println("C is Gr" +c);
+		}
+		
+		
+
+	}
+
+}
